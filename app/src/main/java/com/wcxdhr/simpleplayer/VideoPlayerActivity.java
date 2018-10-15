ed41aa1cc8455e3fee60b097dfa014ab0a7d8888
wcxdhr <wangchunxuedhr@bupt.edu.cn>
2018-10-15 12:07:28 +0800

    Third commit 2.0

--- app/src/main/java/com/wcxdhr/simpleplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/wcxdhr/simpleplayer/VideoPlayerActivity.java
@@ -1,13 +1,25 @@
 package com.wcxdhr.simpleplayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
 
 public class VideoPlayerActivity extends AppCompatActivity {
+
+    private VideoView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,43 @@ public class VideoPlayerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        test = (VideoView) findViewById(R.id.video_view);
+
+        test.setMediaController(new MediaController(this));
+
+        if (ContextCompat.checkSelfPermission(VideoPlayerActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(VideoPlayerActivity.this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }else {
+            initVideoPath();
+        }
+
+        test.start();
+
+
+
+    }
+
+    private void initVideoPath(){
+        File file = new File(Environment.getExternalStorageDirectory(), "Screenrecord-2017-07-04-00-05-58-593.mp4");
+        test.setVideoPath(file.getPath());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initVideoPath();
+                }else{
+                    Toast.makeText(this, "拒绝了", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
     }
 
 }
--- app/src/main/java/com/wcxdhr/simpleplayer/db/Video.java
+++ app/src/main/java/com/wcxdhr/simpleplayer/db/Video.java
@@ -3,4 +3,34 @@ package com.wcxdhr.simpleplayer.db;
 import org.litepal.crud.LitePalSupport;
 
 public class Video extends LitePalSupport {
+
+    private String name;
+
+    private String author;
+
+    private int count;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
